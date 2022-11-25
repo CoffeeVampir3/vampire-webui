4aeaa78883544409fdff8d76f5122092db20e2b6
B <you@example.com>
2022-11-25 00:04:38 -1000

    Pipeline now is a state, much faster.

--- diffusion_pipeline.py
+++ diffusion_pipeline.py
@@ -1,25 +1,14 @@
 import torch
 import tqdm
 from torch import autocast
-from diffusers import StableDiffusionPipeline
+from diffusers import StableDiffusionPipeline, EulerDiscreteScheduler
 
-
-
-def no_nsfw_filter(images, clip_input):
-    return images, False
-
-def testpipeline(prompt, neg_prompt, seed, num_images, width, height, num_steps, cfg):
-    pipe = StableDiffusionPipeline.from_pretrained("./content/stable-diffusion-2-base", safety_checker=None)
+def testpipeline(pipe, prompt, neg_prompt, seed, num_images, width, height, num_steps, cfg):
     print(pipe)
-    pipe = pipe.to("cuda")
-
-    print(pipe)
-
     generator = torch.Generator("cuda").manual_seed(seed)
     torch.cuda.empty_cache()
-    torch.cuda.synchronize()
 
-    print(f'Prompt: {prompt}, negatives: {neg_prompt}, nimages: {num_images}, w: {width}, h: {height}, steps: {num_steps}, cfg: {cfg}')
+    print(f'Prompt: {prompt}, negatives: {neg_prompt}, seed: {seed}, nimages: {num_images}, w: {width}, h: {height}, steps: {num_steps}, cfg: {cfg}')
     multi_prompt = [prompt] * num_images
     multi_negative_prompt = [neg_prompt] * num_images
     with autocast("cuda"):
@@ -27,4 +16,14 @@ def testpipeline(prompt, neg_prompt, seed, num_images, width, height, num_steps,
         images = out.images
         del out
     
-    return images
+    return images, pipe
+
+def load_pipeline():
+    model_id = "stable-diffusion-2"
+    model_path = f"./content/{model_id}"
+    scheduler = EulerDiscreteScheduler.from_pretrained(model_path, subfolder="scheduler")
+    pipe = StableDiffusionPipeline.from_pretrained(model_path, safety_checker=None, scheduler=scheduler)
+    pipe = pipe.to("cuda")
+    torch.cuda.empty_cache()
+    torch.cuda.synchronize()
+    return pipe
--- webui.py
+++ webui.py
@@ -1,17 +1,18 @@
 import gradio as gr
 import diffusion_pipeline as dp
 
-prompt_textbox = gr.inputs.Textbox(lines=3, placeholder="")
-negative_prompt_textbox = gr.inputs.Textbox(lines=3, placeholder="")
-seed = gr.inputs.Number(default=12345,label="Seed")
-num_inputs_slider = gr.inputs.Slider(minimum=1, maximum=100,default=1,step=1)
-width_slider = gr.inputs.Slider(minimum=256, maximum=2048,default=512,step=64)
-height_slider = gr.inputs.Slider(minimum=256, maximum=2048,default=512,step=64)
-num_steps_slider = gr.inputs.Slider(minimum=1, maximum=250,default=1,step=1)
-cfg_slider = gr.inputs.Slider(minimum=1, maximum=30,default=7,step=0.5)
+prompt_textbox = gr.Textbox(lines=3, placeholder="")
+negative_prompt_textbox = gr.Textbox(lines=3, placeholder="")
+seed = gr.Number(value=12345,label="Seed",precision=0)
+num_inputs_slider = gr.Slider(minimum=1, maximum=100,value=1,step=1)
+width_slider = gr.Slider(minimum=256, maximum=2048,value=512,step=64)
+height_slider = gr.Slider(minimum=256, maximum=2048,value=512,step=64)
+num_steps_slider = gr.Slider(minimum=1, maximum=250,value=1,step=1)
+cfg_slider = gr.Slider(minimum=1, maximum=50,value=7,step=0.5)
 
-output_img = gr.Gallery(type="pil")
+output_img = gr.Gallery()
 
-app_inputs = [prompt_textbox, negative_prompt_textbox, seed, num_inputs_slider, width_slider, height_slider, num_steps_slider, cfg_slider]
-app = gr.Interface(fn=dp.testpipeline, inputs=app_inputs, outputs=output_img, allow_flagging="never")
+pipeline = gr.State(value = dp.load_pipeline)
+app_inputs = [pipeline, prompt_textbox, negative_prompt_textbox, seed, num_inputs_slider, width_slider, height_slider, num_steps_slider, cfg_slider]
+app = gr.Interface(fn=dp.testpipeline, inputs=app_inputs, outputs=[output_img, pipeline], allow_flagging="never")
 app.launch()
