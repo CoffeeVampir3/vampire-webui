d96133735f9e039493c03bcbc47f7f822698f8e2
B <you@example.com>
2022-11-26 03:08:58 -1000

    Restructured project.

---
+++ launch.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+conda activate ldm
+python ui/webui.py
+
+#!/usr/bin/sh
+$SHELL
---
+++ ui/ui_config.py
@@ -0,0 +1,31 @@
+from omegaconf import OmegaConf
+from pathlib import Path
+from os import walk
+
+def save_ui_config(**kwargs):
+    config = OmegaConf.create(kwargs)
+    dir_path = Path("./configs")
+    file_name = "last_run.yaml"
+    if not dir_path.exists():
+        dir_path.mkdir()
+    dest = (dir_path/file_name)
+    OmegaConf.save(config=config, f=dest)
+
+def load_ui_config():
+    dir_path = Path("./configs")
+    file_name = "last_run.yaml"
+    dest = (dir_path/file_name)
+
+    if not dest.exists():
+        return ("stable-diffusion-2", "DPM", "", "", 1, 1, 1, 768, 768, 20, 7)
+
+    config = OmegaConf.load(dest)
+    return list(config.values())
+
+def enumerate_models():
+    dir_path = Path("./content")
+    if not dir_path.exists():
+        return None
+
+    dirs = next(walk(dir_path))[1]
+    return dirs
